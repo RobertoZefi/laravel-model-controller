b2aa52a7bd14bb8f3d417ff417b9136956a2cdda
RobertoZefi <zefi.roberto2001@gmail.com>
2023-04-11 17:56:40 +0200

    FEAT movies added

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('welcome');
+        $movies = Movie::all();
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('welcome', $data);
     }
 }
